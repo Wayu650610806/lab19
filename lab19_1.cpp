bbeb1840a44cd60f37ead5c52c076ea013b8bf8c
Wayu Tharai <iarahtuyaw@gmail.com>
2023-02-17 19:54:35 +0700

    i forgot &

--- lab19_1.cpp
+++ lab19_1.cpp
@@ -20,47 +20,50 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(string file,vector<string> n,vector<int> s,vector<char> g){
+void importDataFromFile(string file,vector<string> &n,vector<int> &s,vector<char> &g){
     ifstream f("name_score.txt");
     string input;
     int i=0;
+    int a,b,c;
+        char name[100];
     while (getline(f,input))
-    {   int a,b,c;
-        string name;
-        // const char * in = input.c_str();
+    {
+        
         char format[]="%[^:]: %d %d %d";
         sscanf(input.c_str(),format,name,&a,&b,&c);
         n.push_back(name);
-        s.push_back(a+b+c);
+        int sum=a+b+c;
+        s.push_back(sum);
         g.push_back(score2grade(s[i]));
+        
         i++;
     }
     
 }
 
+
 void getCommand(string &c,string &key){
-    // string input,com,k;
+    string input;
+    char comman[100];
+    char k[100];
     cout<<"Please input your command: ";
-    cin>>c;
-    cin>>key;
-    // getline(cin,input);
-    // char format[] = "%s %s";
-    // // const char * in = input.c_str();
-    // sscanf(input.c_str(),format,&com,&k);
-    // key=k;
-    // c=com;
+    getline(cin,input);
+    char format[] = "%s %[^\n]";
+    sscanf(input.c_str(),format,comman,k);
+    c=comman;
+    key=k;
 }
 
 void searchName(vector<string> n,vector<int> s,vector<char> g,string k){
     int count=0;
     cout << "---------------------------------\n";
-    cout<<n[0];
-    for(int i=0;i<10;i++){
-        toUpperStr(n[i]);
-        cout<<n[i];
-        if(k==n[i]){
+    for(unsigned int i=0;i<n.size();i++){
+        string name=n[i];
+        if(k==toUpperStr(name)){
             count++;
-            cout<<n[i]<<" ("<<s[i]<<")\n";
+            cout<<n[i]<<"'s score = "<<s[i]<<"\n";
+            cout<<n[i]<<"'s grade = "<<g[i]<<"\n";
+            
         }
     }
     if(count==0) cout << "Cannot found.\n";
@@ -74,24 +77,26 @@ void searchGrade(vector<string> n,vector<int> s,vector<char> g,string k){
     const char * key = k.c_str();
     char keychar=key[0];
     cout << "---------------------------------\n";
-    for(int i=0;i<n.size();i++){
+    for(unsigned int i=0;i<n.size();i++){
         if(keychar==g[i]){
+            
             count++;
-            cout<<n[i]<<"'s score = "<<s[i]<<"\n";
-            cout<<n[i]<<"'s grade = "<<g[i]<<"\n";
+            cout<<n[i]<<" ("<<s[i]<<")\n";
         }
     }
     if(count==0) cout << "Cannot found.\n";
     cout << "---------------------------------\n";
    
 }
+
+
 int main(){
     string filename = "name_score.txt";
     vector<string> names;
     vector<int> scores;
     vector<char> grades; 
     importDataFromFile(filename, names, scores, grades);
-    int i=0;
+    
     do{
         string command, key;
         getCommand(command,key);
@@ -105,8 +110,7 @@ int main(){
             cout << "Invalid command.\n";
             cout << "---------------------------------\n";
         }
-        i++;
-    }while(i<1);
+    }while(true);
     
     return 0;
 }
